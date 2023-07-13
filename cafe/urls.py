77e2e36fef9b568f78629e6a74f5f4e3a31db95b
Pablo Avendaño <79715881+JackStar6677@users.noreply.github.com>
2023-07-13 09:32:54 -0400

    Update urls.py

--- cafe/urls.py
+++ cafe/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('login/', views.login, name='login'),
     path('pagina2/', views.pagina2, name='pagina2'),
     path('pagina3/', views.pagina3, name='pagina3'),
+    path('carrito/', views.carrito, name='carrito'),
     
     path('productos/', views.lista_productos, name='lista_productos'),
     path('productos/crear/', views.crear_producto, name='crear_producto'),
